e3c89f3529bd4dbee31bd5f070cbb0a0447894c3
Ethel C <cofresi.ethel@gmail.com>
2018-09-07 18:12:40 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,15 +4,14 @@ class Application
     resp = Rack::Response.new
  
 
- 
+   if  
     if num_1==num_2 && num_2==num_3
-      resp.write "You Win"
+      resp.write "Good Morning!"
     else
-      resp.write "You Lose"
+      resp.write "Good Afternoon!"
     end
  
     resp.finish
   end
  
 end
-#express output to our user. Because this is the web, that means adding it to our
